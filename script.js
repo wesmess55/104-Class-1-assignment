0c92c72f6d246d58be0bf5cd01e5b4ced9abd29e
wesmess55 <66344929+wesmess55@users.noreply.github.com>
2020-06-30 21:00:44 -0500

    Add files via upload
    average 6.6666

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("executed");
+
+
+var carName="Volvo";
+console.log(carName);
+
+var x=50;
+console.log(x);
+
+let band="Metalica", song = "Enter sandman";
+
+let name;
+
+name=band + " : " + song;
+console.log(name.toUpperCase()); 
+console.log(name.length);
+
+
+let activity = "I am learning JavaScript in this course";
+name=activity.split(" ");
+console.log(name);
+
+let interests = "programming, learning, music, volleyball, series";
+name= interests.split(",");
+console.log(name);
+
+name=activity.includes("JavaScript");
+console.log(name);
+
+email="wesmess55@gmail.com";
+name=email.includes("@");
+
+console.log(name);
---
+++ script2.js
@@ -0,0 +1,26 @@
+//arrays
+
+const months = new Array("Jan,Feb,Mar,Apr");
+
+months.push("May");// push adds elemnet at end
+
+months.unshift("Month 0");//new element at beginning
+
+months[3]= "March";
+
+console.table(months);
+
+//alert("months");
+
+var numbers = [10,2,5,8,9,6];
+// sum the numbers
+
+var total= numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5];
+
+var average= total/6
+
+alert(average)
+
+
+
+
